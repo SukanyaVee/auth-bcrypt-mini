e594cdd8586b2ed57ad2eb4d2bfac47ed2e6e723
unknown <you@example.com>
2018-01-10 12:15:20 -0700

    Jan 10 brcrypt mini

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyPaser = require('body-parser');
 const session = require('express-session');
 const massive = require('massive');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 12;
 
 require('dotenv').config();
 const app = express();
@@ -16,11 +19,36 @@ app.use(session({
 app.use(express.static(`${__dirname}/../build`));
 
 app.post('/register', (req, res) => {
-  // Add code here
+    // Add code here
+  const db = app.get('db')
+  bcrypt.hash(req.body.password, saltRounds).then(hashedPassword => {
+    db.create_user([req.body.username, hashedPassword]).then(()=>
+      {
+        req.session.user = req.body.username;
+        res.json({user:req.session.user})
+      }).catch(error => {
+        console.log('error',error);
+        res.status(500).json({message: 'Something went wrong'})
+      })
+  })
 });
 
 app.post('/login', (req, res) => {
-  // Add code here
+  //add code here
+  const db = app.get('db');
+  const { username, password } = req.body;
+  db.find_user([username]).then(users => {
+    if (users.length) {
+      if (users[0].password === password) {
+        req.session.user = { username: users[0].username };
+        res.json({ user: req.session.user });
+      } else {
+        res.status(403).json({ message: 'Wrong password' })
+      }
+    } else {
+      res.status(403).json({ message: "That user is not registered" })
+    }
+  })
 });
 
 app.post('/logout', (req, res) => {
